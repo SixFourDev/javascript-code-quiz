75bf649936321f03ba355d73bf0f048ee5715cee
Shane <sb_94@yahoo.com>
2023-04-22 12:52:59 -0700

    added comments to html

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// start quiz button
+
+// timer starts and presented a question
+
+// answer question then get presented another question
+// answer a question incorrectly time subtracted from clock
+
+// game over when all questions answered or timer reaches 0
+
+// save my initials and score
